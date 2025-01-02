89b8d3ca4ae1fd528f63a572ed5017d954873066
Vladislav Semenov <74904348+Vladislav-IS@users.noreply.github.com>
2025-01-03 00:49:55 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,14 +3,14 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     # URL сервера при локальном запуске Streamlit и FastAPI
-    FASTAPI_URL: str = "http://127.0.0.1:8000/"
+    # FASTAPI_URL: str = "http://127.0.0.1:8000/"
 
     # URL сервера при запуске Streamlit в Docker
     # FASTAPI_URL: str = "http://fastapi:8000/"
 
     # URL сервера при запуске на VPS
-    # FASTAPI_URL: str =\
-    #     'https://ai-year-project-service-qeke.onrender.com/'
+    FASTAPI_URL: str =\
+        'https://ai-year-project-service-qeke.onrender.com/'
 
     # добавка к URL
     ROUTE: str = "api/model_service/"
